1d6f66f7343023043836d6a3ded3ac7fffa304d3
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-06-03 11:40:22 +0200

    Update transform_script_tags.ts

--- src/transform_script_tags.ts
+++ src/transform_script_tags.ts
@@ -1,8 +1,19 @@
 import {Plugin} from "@esbuild";
 
-const transformScriptTags : (
-  options? : {readTextFileSync: (path: string | URL) => string}
-) => Plugin = (options = {readTextFileSync: Deno.readTextFileSync}) => ({
+interface Options {
+  /**
+   * The reader to use to read the contents of a file. Defaults to `Deno.readTextFileSync`.
+   */
+  readTextFileSync: (path : string | URL) => string;
+}
+
+/**
+ * Transforms the `src` attribute of script tags in HTML files to point to JavaScript files instead of TypeScript files.
+ * 
+ * @param options Options for the plugin
+ * @returns A plugin object that can be used with esbuild.
+ */
+const transformScriptTags : (options? : Options) => Plugin = (options = {readTextFileSync: Deno.readTextFileSync}) => ({
   name: 'transform-script-tags',
   setup(build) {
     build.onLoad({filter: /\.html$/}, (args) => {
